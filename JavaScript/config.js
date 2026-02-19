8363fae69e932e48a6d060fbb8a8e3acf86ec4c5
Seb_stiaN <135262776+Boterazo@users.noreply.github.com>
2026-02-18 20:38:48 -0500

    Update config.js

--- JavaScript/config.js
+++ JavaScript/config.js
@@ -1,3 +1,4 @@
 export const IMG_PERFIL = "image/perfil.png";
 
-export const URL_END_POINT = "https://fastapi-gfrd.onrender.com";
+export const URL_END_POINT = "https://fastapi-hvgi.onrender.com";
+
